030ea9bc754e7cb3246f606869de080605890acb
jaluspeet <jaluspit@gmail.com>
2024-10-22 11:15:16 +0200

    Move main() inside ComplexNumber class

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -14,11 +14,11 @@ class ComplexNumber {
     public double getReal() {
         return this.real;
     }
-    
+
     public ComplexNumber plus(final ComplexNumber other) {
         return new ComplexNumber(this.getReal() + other.getReal(), this.getImaginary() + other.getImaginary());
     }
-    
+
     public ComplexNumber sub(final ComplexNumber other) {
         return new ComplexNumber(this.getReal() - other.getReal(), this.getImaginary() - other.getImaginary());
     }
@@ -38,33 +38,35 @@ class ComplexNumber {
     public String toString(){
         return "Numero complesso: " + this.getReal() + " " + this.getImaginary() + "j";
     }
+
+    public static void main(String[] args) {
+        System.out.println(new ComplexNumber(4, 8).toString());
+        ComplexNumber num1 = new ComplexNumber(2, 4);
+        ComplexNumber num2  = new ComplexNumber(3, 8);
+
+        if (num1.plus(num2) == new ComplexNumber(5, 12)) {
+            System.out.println("Sum Test Passed");
+        }else{
+            System.out.println("Sum Test Failed");
+        }
+
+        if(num1.sub(num2) == new ComplexNumber(-1, -4)){
+            System.out.println("Sub Test Passed");
+        }else{
+            System.out.println("Sub Test Failed");
+        }
+
+        ComplexNumber z1 = new ComplexNumber(3, 2);
+        ComplexNumber z2 = new ComplexNumber(1, 4);
+
+        ComplexNumber result = z1.times(z2);
+
+        if(result == new ComplexNumber(-5, 14)){
+            System.out.println("test times: true");
+        }else{
+            System.out.println("test times: false");
+        }
+    }
 }
 
-public static void main(String[] args) {
-    System.out.println(new ComplexNumber(4, 8).toString());
-    ComplexNumber num1 = new ComplexNumber(2, 4);
-    ComplexNumber num2  = new ComplexNumber(3, 8);
 
-    if (num1.plus(num2) == new ComplexNumber(5, 12)) {
-        System.out.println("Sum Test Passed");
-    }else{
-        System.out.println("Sum Test Failed");
-    }
-
-    if(num1.sub(num2) == new ComplexNumber(-1, -4)){
-        System.out.println("Sub Test Passed");
-    }else{
-        System.out.println("Sub Test Failed");
-    }
-
-    ComplexNumber z1 = new ComplexNumber(3, 2);
-    ComplexNumber z2 = new ComplexNumber(1, 4);
-
-    ComplexNumber result = z1.times(z2);
-
-    if(result == new ComplexNumber(-5, 14)){
-        System.out.println("test times: true");
-    }else{
-        System.out.println("test times: false");
-    }
-}
